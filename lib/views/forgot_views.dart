5b121914d45c2e13fac846fc0553c66f31447d47
andresaftari <andresaftari@gmail.com>
2024-03-28 01:04:06 +0700

    Adding architecture & color theme

---
+++ lib/views/forgot_views.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassPageScreen extends StatefulWidget {
+  const ForgotPassPageScreen({super.key});
+
+  @override
+  State<ForgotPassPageScreen> createState() => _ForgotPassPageScreenState();
+}
+
+class _ForgotPassPageScreenState extends State<ForgotPassPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
